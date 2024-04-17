c48f031eaefb8aa9a0735b3b673cdae802f6ae5c
cilantech <isazaherbin@gmail.com>
2024-04-17 07:22:11 -0500

    convertir y saber tipo de numeros

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+//////////////////////formas de operar u convertir numeros//////////////////////
+// let myFirtsNumber = 5
+// let mySecondNumber = "2"
+// mySecondNumber=Number(mySecondNumber)
+
+// parseInt(mySecondNumber)
+
+// //document.write(myFirtsNumber)
+
+// console.info(myFirtsNumber*mySecondNumber)
+// //console.info(myFirtsNumber+ Number(mySecondNumber))
+// console.info(myFirtsNumber+mySecondNumber)
+// console.info(myFirtsNumber-mySecondNumber)
+// console.info(myFirtsNumber/mySecondNumber)
+
+
+
+//////////////////////saber el tipo de numero//////////////////////
+// let myFirtsNumber = 3.1416
+// document.write(myFirtsNumber)
+
+
+// console.info(typeof(myFirtsNumber))
+// console.info(Number.isInteger(myFirtsNumber))
+
+const numberOne = 50000
+const numberTwo = 50000
