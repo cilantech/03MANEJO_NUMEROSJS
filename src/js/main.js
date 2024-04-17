877cf66c92ab308ac9c84aff1c236c0e0be1e708
cilantech <isazaherbin@gmail.com>
2024-04-17 09:05:48 -0500

    multiples operaciones

--- src/js/main.js
+++ src/js/main.js
@@ -23,14 +23,69 @@
 // console.info(typeof(myFirtsNumber))
 // console.info(Number.isInteger(myFirtsNumber))
 
-const numberOne = 500000000000
-const numberTwo = 500_000_000_000
+// const numberOne = 500000000000
+// const numberTwo = 500_000_000_000
 
-console.info(numberOne+numberTwo)
-console.info(numberOne-numberTwo)
-console.info(numberOne*numberTwo)
-console.info(numberOne/numberTwo)
-console.info(Number.isFinite(numberOne*numberTwo))
-console.info(Number.MAX_VALUE) // saber valor maximo
-console.info(Number.MIN_VALUE) // saber valor minimo
-console.info(Math.PI)
+// console.info(numberOne+numberTwo)
+// console.info(numberOne-numberTwo)
+// console.info(numberOne*numberTwo)
+// console.info(numberOne/numberTwo)
+// console.info(Number.isFinite(numberOne*numberTwo))
+// console.info(Number.MAX_VALUE) // saber valor maximo
+// console.info(Number.MIN_VALUE) // saber valor minimo
+// console.info(Math.PI)
+
+// const exampleNumber = 80055
+
+// console.info(exampleNumber)
+// console.info(exampleNumber.toExponential(5))
+// console.info(exampleNumber.toString(2))
+// console.info(exampleNumber.toString(16))
+
+// const example = 5.1265456165
+
+// document.write(example.toFixed(0))
+// console.info(example.toFixed(1))
+// console.info(example.toFixed(2))
+// console.info(example.toFixed(3))
+
+// const numberForRadius = 3.4999
+
+// document.write(numberForRadius)
+
+// console.info(Math.round(numberForRadius))
+// console.info(Math.floor(numberForRadius))
+// console.info(Math.ceil(numberForRadius))
+// console.info(Math.trunc(numberForRadius))
+// console.info(Number.parseInt(numberForRadius))
+// console.info(Number.parseFloat(numberForRadius))
+
+
+// const numeroAleatorio = parseInt(Math.random()*100)
+// const numeroAleatorio = Math.trunc(Math.random()*100)
+// document.write(numeroAleatorio)
+
+//let num1 = 50
+
+//let num2 = 80
+
+
+//(valor superior - valor inferior +1) 
+
+//const numeroAleatorioConRango = Math.trunc(Math.random()*(100-50+1)+50)
+
+//document.write(numeroAleatorioConRango)
+
+// console.info(Math.floorMath.random()*(num2 + 1)+num1)
+
+//const numeroAleatorioConRango = Math.round(Math.random()*(92-90+1)+90)
+
+// console.log(Math.abs(-50))
+
+// console.log(2*2*2*2*2)
+
+// console.log(Math.pow(2,80))
+
+// console.log(Math.sqrt(19491))
+// console.log(Math.cbrt(545))
+
